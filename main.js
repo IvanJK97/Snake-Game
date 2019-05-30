593d8585e8a46d8a75a3ddc97d62163cbaef697c
IvanJayK97 <ivk215@lehigh.edu>
2019-05-29 22:03:12 -0400

    Changed boundary to non-wrap, need to fix positioning of snake body following the head

--- main.js
+++ main.js
@@ -32,10 +32,10 @@ function inputKeyPressed(event) {
         if (lastMovement == "Up" || lastMovement == "Down") {
             // Can't move vertically if already moving vertically
         } else {
-        clearInterval(intervalFunction);
-        setTimeout(downMovement, 250);
-        intervalFunction = setInterval(downMovement, 500);
-        lastMovement = "Down";
+            clearInterval(intervalFunction);
+            setTimeout(downMovement, 250);
+            intervalFunction = setInterval(downMovement, 500);
+            lastMovement = "Down";
         }
     }
     else if (keyCode == buttonKeys.LEFTARROW) {
@@ -62,13 +62,14 @@ function inputKeyPressed(event) {
     }
     else if (keyCode == buttonKeys.SPACEBAR) {
         // alert("Paused! Press arrow keys to restart.");
+        lastMovement = "Space"
         clearInterval(intervalFunction);
     }
     else if (keyCode == buttonKeys.ENTER) {
         increaseSize();
     }
     else {
-
+        // Maybe start the game with down
     }
 }
 
@@ -80,34 +81,46 @@ function increaseSize() {
 
     const index = snakeArray.length + 1;
     snakeArray.push(index);
+    // storePositionOfBody(xHeadPos, yHeadPos); // TODO: storeBodyPositionHere
 
-    const xBodyPos = calculatePositionOfNewBody(xHeadPos, yHeadPos, lastMovement).x;
-    const yBodyPos = calculatePositionOfNewBody(xHeadPos, yHeadPos, lastMovement).y;
+    const xBodyPos = calculatePositionOfNewBody(xHeadPos, yHeadPos).x;
+    const yBodyPos = calculatePositionOfNewBody(xHeadPos, yHeadPos).y;
 
-    console.log(xBodyPos);
     // https://stackoverflow.com/questions/5677799/how-to-append-data-to-div-using-javascript
     // TODO: make width dynamically?
     element.innerHTML += '<div id="body' + index + '" style="background-color: orange; position:absolute; width:18px; height:18px; margin-left:' + xBodyPos + 'px; margin-top:' + yBodyPos + 'px; border: black solid 1px;"></div>';
-    console.log("ran");
 }
 
-function calculatePositionOfNewBody(headX, headY, lastMovement) {
+function calculatePositionOfNewBody(headX, headY) {
     const element = document.querySelector('.player');
     const style = getComputedStyle(element);
     
-    const width = parseInt(style.width);
+    const blockWidth = parseInt(style.width);
     const size = snakeArray.length;
 
+    // Using 20 instead of blockWidth, doesn't appear to matter visually??
     if (lastMovement == "Right") {
-        let xBodyPos = (width) * size;
+        let xBodyPos = 20 * size;
         xBodyPos = headX - xBodyPos;
         const yBodyPos = headY;
         return {x: xBodyPos, y: yBodyPos}
-    } else {
-        let xBodyPos = (width + offSet) * size;
-        xBodyPos += headX;
+    } else if (lastMovement == "Left") {
+        let xBodyPos = blockWidth * size;
+        xBodyPos = headX + xBodyPos;
         const yBodyPos = headY;
         return {x: xBodyPos, y: yBodyPos}
+    } else if (lastMovement == "Up") {
+        let yBodyPos = 20 * size;
+        yBodyPos = headY + yBodyPos;
+        const xBodyPos = headX;
+        return {x: xBodyPos, y: yBodyPos}
+    } else if (lastMovement == "Down") {
+        let yBodyPos = 20 * size;
+        yBodyPos = headY - yBodyPos;
+        const xBodyPos = headX;
+        return {x: xBodyPos, y: yBodyPos}
+    } else {
+        // Maybe start the game off with downmovement?
     }
 }
 
@@ -121,41 +134,79 @@ function calculatePositionOfHead() {
     return {x: xPos, y: yPos};
 }
 
+function rightMovement() {
+    const headPosition = calculatePositionOfHead();
+    const xPos = headPosition.x;
+    const yPos = headPosition.y;
+
+    const tailPosition = calculatePositionOfNewBody(xPos, yPos);
+    const xTailPos = tailPosition.x;
+    const yTailPos = tailPosition.y;
+
+
+    // console.log(xPos);
+    // Keep this as 20, width of player + outline
+    let newXPos = xPos + 20;
+
+    // TODO: maybe set blockWidth as global var for DRY, or maybe get rid
+    const element = document.querySelector('.player');
+    const style = getComputedStyle(element);
+    const blockWidth = parseInt(style.width);
+    
+    const playerRight = newXPos + 20;
+    // check if player's right edge is outside of screen
+    if (playerRight > 500) {
+        alert("Game over!");
+        clearInterval(intervalFunction);
+        newXPos = xPos;
+        // newXPos = 0;
+    }
+
+    document.getElementById('player').style.marginLeft = newXPos + 'px';
+    for (let i = 1; i <= snakeArray.length; i++) {
+        const bodyXPos = newXPos - (i * 20);
+        document.getElementById('body' + i).style.marginLeft = bodyXPos + 'px';
+        
+        // console.log(yPos);
+        // console.log(yTailPos); Didn't work
+        // TODO, need two movements ago or use tailPosition
+        if (yPos > yTailPos) { // Two movements ago was "Up"
+            const bodyYPos = yPos - (i * 20);
+            document.getElementById('body' + i).style.marginTop = bodyYPos + 'px';
+        } else if (yPos < yTailPos) { // Two movements ago was "Down"
+            console.log("ran");
+            const bodyYPos = yPos + (i * 20);
+            document.getElementById('body' + i).style.marginTop = bodyYPos + 'px';
+        } else {
+
+        }
+    }
+}
+
 // https://zellwk.com/blog/css-values-in-js/
 function leftMovement() {
     const xPos = calculatePositionOfHead().x;
     // console.log(xPos);
     let newXPos = xPos - 20;
 
+    // TODO: maybe set blockWidth as global var for DRY
+    const element = document.querySelector('.player');
+    const style = getComputedStyle(element);
+    const blockWidth = parseInt(style.width);
+
     const playerLeft = newXPos;
     // check if player's left edge is outside of screen
     if (playerLeft < 0) {
-        newXPos = 480;
+        alert("Game over!");
+        // Stop the player and set position to edge of screen
+        clearInterval(intervalFunction);
+        newXPos = xPos;
+        // newXPos = 480;
     }
 
-    document.getElementById('player').style.marginLeft = newXPos + 'px';
-}
-
-function rightMovement() {
-    const xPos = calculatePositionOfHead().x;
-    console.log(xPos);
-    let newXPos = xPos + 20;
-
-    // TODO: maybe set width as global var for DRY
-    const element = document.querySelector('.player');
-    const style = getComputedStyle(element);
-    const width = parseInt(style.width);
-    
-    const playerRight = newXPos + width;
-    // check if player's right edge is outside of screen
-    if (playerRight > 500) {
-        newXPos = 0;
-    }
-
-    // TODO: have to set new right edge, do a border maybe
     document.getElementById('player').style.marginLeft = newXPos + 'px';
     for (let i = 1; i <= snakeArray.length; i++) {
-        const bodyXPos = newXPos - (i * width);
+        const bodyXPos = newXPos + (i * 20);
         document.getElementById('body' + i).style.marginLeft = bodyXPos + 'px';
     }
 }
@@ -167,10 +218,17 @@ function upMovement() {
     const playerTop = newYPos;
     // check if player's top edge is outside of screen
     if (playerTop < 0) {
-        newYPos = 480;
+        alert("Game over!");
+        clearInterval(intervalFunction);
+        newYPos = yPos;
+        // newYPos = 480;
     }
 
     document.getElementById('player').style.marginTop = newYPos + 'px';
+    for (let i = 1; i <= snakeArray.length; i++) {
+        const bodyYPos = newYPos + (i * 20);
+        document.getElementById('body' + i).style.marginTop = bodyYPos + 'px';
+    }
 }
 
 function downMovement() {
@@ -181,8 +239,15 @@ function downMovement() {
     const playerBottom = newYPos + 20;
     // check if player's bottom edge is outside of screen
     if (playerBottom > 500) {
-        newYPos = 0;
+        alert("Game over!");
+        clearInterval(intervalFunction);
+        newYPos = yPos;
+        // newYPos = 0;
     }
 
     document.getElementById('player').style.marginTop = newYPos + 'px';
+    for (let i = 1; i <= snakeArray.length; i++) {
+        const bodyYPos = newYPos - (i * 20);
+        document.getElementById('body' + i).style.marginTop = bodyYPos + 'px';
+    }
 }
